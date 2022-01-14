d336fa55e0b25a024850098b0954b78251a99793
aaronk2002 <65597169+aaronk2002@users.noreply.github.com>
2022-01-14 18:23:34 -0500

    Add files via upload

---
+++ skeleton-master/client/src/components/pages/SignIn.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "./SignIn.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+
+const SignIn = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <>
+      {userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
+      <div>
+          Title
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
