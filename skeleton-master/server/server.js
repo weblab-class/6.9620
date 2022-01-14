b2679476c24143eeadfb692f7ed9da72ded3a186
aaronk2002 <65597169+aaronk2002@users.noreply.github.com>
2022-01-14 17:17:56 -0500

    Update server.js

--- skeleton-master/server/server.js
+++ skeleton-master/server/server.js
@@ -33,9 +33,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://fntava-aaronk2002:weblab2022@cluster0.bbxlf.mongodb.net/Cluster0?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "Cluster0";
 
 // connect to mongodb
 mongoose
