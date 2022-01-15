18f2bbb9acd1d61d18c28eec5f0f1c48b4d8c6c8
aaronk2002 <65597169+aaronk2002@users.noreply.github.com>
2022-01-15 12:02:15 -0500

    Add files via upload

---
+++ skeleton-master/client/src/components/pages/Game.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { get } from "../../utilities"
+import "./Game.css";
+
+const Game = (props) => {
+  return (
+    <>
+      Game
+    </>
+  );
+};
+
+export default Game;
---
+++ skeleton-master/client/src/components/pages/Leaderboard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { get } from "../../utilities"
+import "./Leaderboard.css";
+
+const Leaderboard = (props) => {
+  return (
+    <>
+      Leaderboard
+    </>
+  );
+};
+
+export default Leaderboard;
