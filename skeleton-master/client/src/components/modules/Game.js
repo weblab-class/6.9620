e1e98e76ee4617aea75e775b67e4005cc8a2b9a8
Aaron Alvarado <aaronakj2002@gmail.com>
2022-01-17 20:15:17 -0500

    added comments on Game.js

--- skeleton-master/client/src/components/modules/Game.js
+++ skeleton-master/client/src/components/modules/Game.js
@@ -4,24 +4,38 @@ import { socket } from "../../client-socket.js";
 import "./Game.css";
 
 const Game = (props) => {
+
+
+  // initialize states
   const [player1Point, setPlayer1Point] = useState(0);
   const [player2Point, setPlayer2Point] = useState(0);
+  // for both players, the current word is always stored in the word state
   const [word, setWord] = useState("");
   const [turn, setTurn] = useState(1);
+
+
+  // getting userId1, for some reason, passing props.pairing.player1.userId.length to the API request does not work
   let userId1 = '';
   for (let i = 0; i < props.pairing.player1.userId.length; i++)
   {
     userId1 += props.pairing.player1.userId[i];
   }
+
+
+  // getting userId2
   let userId2 = '';
   for (let i = 0; i < props.pairing.player2.userId.length; i++)
   {
     userId2 += props.pairing.player2.userId[i];
   }
+
+
+  // splitting the case, whether or not this player is player 1 or player 2, and whether it is player 1's turn or player 2's turn
   if (turn % 2 === 1)
   {
     if (props.pairing.player1.userId === props.userId)
     {
+      // asking for a new word
       useEffect(() => {
         get("/api/getword", {opponent: userId2}).then((word) => {
           setWord(word.word);
@@ -38,6 +52,7 @@ const Game = (props) => {
     }
     else
     {
+      // getting new word
       useEffect(() => {
         const changeWord = (newWord) => {
           setWord(newWord.word);
@@ -61,6 +76,7 @@ const Game = (props) => {
   {
     if (props.pairing.player1.userId === props.userId)
     {
+      // getting new word
       useEffect(() => {
         const changeWord = (newWord) => {
           setWord(newWord.word);
@@ -81,6 +97,7 @@ const Game = (props) => {
     }
     else
     {
+      // asking for a new word
       useEffect(() => {
         get("/api/getword", {opponent: userId1}).then((word) => {
           setWord(word.word);
